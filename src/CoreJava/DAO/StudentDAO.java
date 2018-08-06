bfaeb0604523cf160663ef13dd7b568dce9b01ee
Adam Kim <akim456@gmail.com>
2018-08-06 11:44:16 -0400

    started 5

---
+++ src/CoreJava/DAO/StudentDAO.java
@@ -0,0 +1,22 @@
+package CoreJava.DAO;
+
+import CoreJava.systemInterfaces.StudentDAOI;
+
+public class StudentDAO implements StudentDAOI{
+	
+	public Student getStudentByGmail(String email) {
+		
+		/*NEED TO ADD METHOD BODY
+		This method takes a String as a parameter
+		and queries the database for an Student
+		with such an email and returns a Student Object.*/
+	}
+
+	
+	public boolean validateUser(String passToValidate,String comparablePas) {
+		
+	}
+	
+	
+	
+}
